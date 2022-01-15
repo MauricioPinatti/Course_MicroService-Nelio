aa18f9b91b8bb40897d72841b2c303969e8ec1c1
Mauricio Luiz Pinatti1 <mauriciol.pinatti@ibm.com>
2022-01-14 23:59:41 -0300

    Login, JWT token

---
+++ hr-oauth/src/main/java/com/pinatti/hroauth/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.pinatti.hroauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/*
+ * estamos configurando a autenticação 
+ * é quando a aplicação vai receber as credenciais do usuário
+ * vai chamar o usuário pelo username no banco de dados do microserviço user
+ * compar a senha, ver se bate e ai ele vai atenticar o usuário e devolver o token
+ */
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Override
+	@Autowired
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+	}
+
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	
+}
