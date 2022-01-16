121cede9e90b4726fdf6880b6a03d7b43f10797d
Mauricio Luiz Pinatti1 <mauriciol.pinatti@ibm.com>
2022-01-16 20:01:32 -0300

    db config server

--- hr-oauth/src/main/java/com/pinatti/hroauth/config/SecurityConfig.java
+++ hr-oauth/src/main/java/com/pinatti/hroauth/config/SecurityConfig.java
@@ -3,10 +3,8 @@ package com.pinatti.hroauth.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
 
@@ -38,10 +36,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
 	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-	    http.authorizeRequests().antMatchers(HttpMethod.GET, "/users/search/**").permitAll();
-	}
-	
 }
